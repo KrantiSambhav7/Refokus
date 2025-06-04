a854c67b7f62815a1e23a9172a0ace92c7a12bc4
KrantiSambhav7 <krantisambhav7@gmail.com>
2025-06-04 22:48:39 +0530

    Responsive

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import { FaArrowRightLong } from "react-icons/fa6";
 const Card = ({width , start , para , hover="false"}) => {
   
   return (
-    <motion.div whileHover={{ backgroundColor: hover==="true" &&  "#7443ff" , color: hover==="true" && "white" }} className={`bg-pink-200 text-zinc-800 p-5 rounded-xl ${width} min-h-[60vh] flex flex-col justify-between `}>
+    <motion.div whileHover={{ backgroundColor: hover==="true" &&  "#7443ff" , color: hover==="true" && "white" }} className={`bg-pink-100 text-zinc-800 p-5 rounded-xl ${width} min-h-[60vh] flex flex-col justify-between `}>
         <div className='w-full'>
         <div className='w-full flex  items-center justify-between'>
             <h3>REFOKUS</h3>
